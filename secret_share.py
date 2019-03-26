23c6229f36a7de06726350814fe99d0a0acef479
gavinbarrett <gbarrett@ucdavis.edu>
2019-03-26 16:30:55 -0700

    Fixed printing issues

--- secret_share.py
+++ secret_share.py
@@ -18,6 +18,19 @@ def itb(n):
         binstring += str(tmp)
     return binstring[::-1]
 
+def bta(secretbin):
+    ''' Convert a binary string to an ascii string '''
+    n = 8
+    if len(secretbin) % n != 0:
+        secretbin = pad(secretbin)
+    chars = [secretbin[i:i+n] for i in range(0, len(secretbin), n)]
+    secret = ''
+    for c in chars:
+        a = int(c,2)
+        b = chr(a)
+        secret += b
+    return secret
+
 def find_field(n):
     ''' Calculate a Mersenne prime large enough '''
     mersenne = [2, 3, 5, 7, 13, 17, 19, 31, 61, 89, 107, 127, 521, 607, 1279, 2203, 2281, 3217, 4253, 4423, 9689, 9941, 11213]
@@ -70,67 +83,66 @@ def interpolate(x, xs, ys, field):
     return secret % field
 
 def Share():
-        n = int(input('Enter the number of desired shares: '))
-        k = int(input('\nEnter the number for the desired threshold\n(Mustn\'t exceed the number of shares!): '))
-        if (k > n):
-            print('\nThreshold above number of shares!\nAborting scheme!\n')
-            sys.exit(0)
+    ''' Split secret up into n shares '''
+    n = int(input('Enter the number of desired shares: '))
+    k = int(input('\nEnter the number for the desired threshold\n(Mustn\'t exceed the number of shares!): '))
+    if (k > n):
+        print('\nThreshold above number of shares!\nAborting scheme!\n')
+        sys.exit(0)
     
-        # get the secret
-        plaintext = input('\nEnter the secret you would like to share: ')
+    # get the secret
+    plaintext = input('\nEnter the secret you would like to share: ')
     
-        # convert message to integer
-        secret = int(binascii.hexlify(plaintext.encode('utf-8')),16)
-        print('Processing secret: ' + str(secret)) 
-        # generate a secure field
-        field = find_field(secret)
-    
-        # generate array of randomly selected coefficients
-        coeffs = gen_coeff(k, field)
+    # convert message to integer
+    secret = int(binascii.hexlify(plaintext.encode('utf-8')),16)
+    print('Processing secret: ' + str(secret) + '\n')
 
-        # add secret as coefficient 0
-        coeffs = [secret] + coeffs
-
-        # split secret into shares
-        shares = split_secret(n, k, field, coeffs[::-1], secret)
+    # generate a secure field: # |Z_p| >= |M|
+    field = find_field(secret)
     
-        i = 1
-        for s in shares:
-            print('(' + str(i) + ',' + str(s) + ')')
-            i += 1
+    # generate array of randomly selected coefficients
+    coeffs = gen_coeff(k, field)
+
+    # add secret as coefficient 0
+    coeffs = [secret] + coeffs
+
+    # split secret into shares
+    shares = split_secret(n, k, field, coeffs[::-1], secret)
+    
+    # print shares
+    i = 1
+    for s in shares:
+        print('(' + str(i) + ',' + str(s) + ')\n')
+        i += 1
 
 def Recover():
-        xs = []
-        ys = []
-        share_no = int(input('How many shares do you have? '))
-        for i in range(share_no):
+    ''' Try to recover the secret given k shares '''
+    xs = []
+    ys = []
+    share_no = int(input('How many shares do you have? '))
+    for i in range(share_no):
+        try:
+            x, y = input("Please enter your share: ").split(' ')
+            xs.append(int(x))
+            ys.append(int(y))
+        except:
+            print('Please enter x and y share values, seperated by a space.')
+            print('Exiting scheme')
+            sys.exit(0)
+    # find max y value    
+    max_y = int(max(ys))
+    # find an appropriately sized field
+    field = find_field(max_y)
 
-            try:
-                x, y = input("Please enter your share: ").split(' ')
-                xs.append(int(x))
-                ys.append(int(y))
-            except:
-                print('Please enter x and y share values, seperated by a space.')
-                print('Exiting scheme')
-                sys.exit(0)
-        
-        size = int(max(ys))
-        field = find_field(size)
+    # perform polynomial interpolation to recover the secret
+    secretint = interpolate(0, xs, ys, field)
+    # transform the integer into its corresponding binary
+    secretbin = itb(secretint)
+    # interpret the binary string as an ascii string
+    secret = bta(secretbin)
 
-        # perform polynomial interpolation to recover the secret
-        secretint = interpolate(0, xs, ys, field)
-        # transform the integer into its corresponding binary
-        secretbin = itb(secretint)
-        if len(secretbin) % 8 != 0:
-            secretbin = pad(secretbin)
-        n = 8
-        chars = [secretbin[i:i+n] for i in range(0, len(secretbin), n)]
-        secret = ''
-        for c in chars:
-            a = int(c,2)
-            b = chr(a)
-            secret += b
-        print("The secret is: " + secret)
+    print('The secret is: ' + secret + '\n')
+
 
 def main():
     ''' Attempt to establish a sharing scheme '''
